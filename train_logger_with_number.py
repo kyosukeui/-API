a3d3bbb2678ce8305d5aa882a483448aca96d23e
kyosukeui <128306767+kyosukeui@users.noreply.github.com>
2025-12-08 23:16:41 +0900

    Update train_logger_with_number.py

--- train_logger_with_number.py
+++ train_logger_with_number.py
@@ -1,5 +1,8 @@
 import requests, time, csv, os
 from datetime import datetime, timedelta, timezone
+from pathlib import Path   # ← これを忘れずに！
+
+import pandas as pd
 
 url = "https://buscatch.jp/rt3/unko_map_simple.ajax.php"
 data = {"id": "chitetsu_train", "command": "get_unko_list", "rosen_group_id": "2235"}
